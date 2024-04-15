53612acbab716c3fbb6393f203555475060f6cf3
Aditya Savaliya <adityasavaliya1234@gmail.com>
2024-04-15 12:20:46 +0530

    first commit

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://raah-frontend.onrender.com/",
+      "https://raah-frontend.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
